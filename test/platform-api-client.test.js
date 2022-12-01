642410b77cf4b5b58ee74868eda1f15e05dfc813
Vera Kahn <verakahn@nypl.org>
2022-12-01 12:20:51 -0500

    client tests

--- test/platform-api-client.test.js
+++ test/platform-api-client.test.js
@@ -1,8 +1,17 @@
+const { expect } = require('chai')
+const { instance } = require('../lib/platform-api/client')
+
 describe('platform api client', () => {
-  it('creates a new client if there is not one', () => {
-
+  let client
+  it('creates a client if there is not one', async () => {
+    client = await instance()
+    expect(client).to.be.an('object')
   })
-  it('returns existing client', () => {
-
+  it('returns existing client', async () => {
+    before(() => {
+      client = instance()
+    })
+    const secondClient = await instance()
+    expect(secondClient).to.equal(client)
   })
 })
