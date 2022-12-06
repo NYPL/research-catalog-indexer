6537de2784e6379135f8d852c02e7ca08ec69590
Vera Kahn <verakahn@nypl.org>
2022-12-06 17:19:21 -0500

    finish buildsierrabibs test

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,9 @@ const eventDecoder = require('../lib/event-decoder')
 const index = require('../index')
 const requests = require('../lib/platform-api/requests')
 const { expect } = require('chai')
+const SierraBib = require('../lib/sierra-models/bib')
+const SierraItem = require('../lib/sierra-models/item')
+const SierraHolding = require('../lib/sierra-models/holding')
 
 describe('index handler function', () => {
   let eventDecoderStub
@@ -27,14 +30,30 @@ describe('index handler function', () => {
   describe('buildSierraBib', () => {
     let bibs
     before(() => {
-      const records = Array.from(Array(10).keys()).map((n) => ({ id: `b${n}`, _holdings: [{ id: `h1${n}` }, { id: `h2${n}` }], _items: [{ id: `i1${n}` }, { id: `i2${n}` }] }))
+      // create 10 bibs with two items and two holdings each
+      const records = Array.from(Array(10).keys()).map((n) => {
+        return {
+          id: `b${n}`,
+          _holdings: [{ id: `h1${n}` }, { id: `h2${n}` }],
+          _items: [{ id: `i1${n}` }, { id: `i2${n}` }]
+        }
+      })
       bibs = index.internal.buildSierraBibs(records)
     })
     it('instantiates bibs with items and holdings', () => {
       expect(bibs.filter((bib) => bib._items.length && bib._holdings.length).length).to.equal(10)
+      expect(bibs[0].items()[0]).to.be.instanceOf(SierraItem)
+      expect(bibs[0].holdings()[0]).to.be.instanceOf(SierraHolding)
     })
-    it('holdings and items are attached to bibs', () => {
-
+    it('holdings are attached to bibs', () => {
+      const bibOnHolding = bibs[0].holdings()[0].bibs()
+      expect(bibOnHolding).to.have.length(1)
+      expect(bibOnHolding[0]).to.be.instanceOf(SierraBib)
+    })
+    it('items are attached to bibs', () => {
+      const bibOnItem = bibs[0].items()[0].bibs()
+      expect(bibOnItem).to.have.length(1)
+      expect(bibOnItem[0]).to.be.instanceOf(SierraBib)
     })
   })
 
@@ -51,29 +70,33 @@ describe('index handler function', () => {
   })
 
   describe('lambda callback', () => {
-    eventDecoderStub = stub(eventDecoder, 'decodeRecordsFromEvent').callsFake(async () => {
-      return Promise.resolve({ type: 'Holding', records: [{ nyplSource: 'washington-heights', id: '12345678' }] })
+    eventDecoderStub = (type) => stub(eventDecoder, 'decodeRecordsFromEvent').callsFake(async () => {
+      return Promise.resolve({ type, records: [{ nyplSource: 'washington-heights', id: '12345678' }] })
     })
 
     const callback = spy()
     afterEach(() => {
       callback.resetHistory()
       requests.bibsForHoldingsOrItems.restore()
+      eventDecoder.decodeRecordsFromEvent.restore()
     })
     it('calls lambda callback on error', async () => {
       const error = new Error('meep morp')
+      eventDecoderStub('Item')
       stub(requests, 'bibsForHoldingsOrItems').throws(error)
       await index.handler([], null, callback)
       expect(callback.calledOnce).to.equal(true)
       expect(callback).to.have.been.calledWith(error)
     })
     it('calls lambda callback on no record', async () => {
+      eventDecoderStub('Holding')
       stub(requests, 'bibsForHoldingsOrItems').resolves([])
       await index.handler([], null, callback)
       expect(callback.calledOnce).to.equal(true)
       expect(callback).to.have.been.calledWith(null, 'Nothing to do.')
     })
     it('calls lambda callback on successful indexing', async () => {
+      eventDecoderStub('Item')
       stub(requests, 'bibsForHoldingsOrItems').resolves([{ id: '1' }])
       await index.handler([], null, callback)
       expect(callback.calledOnce).to.equal(true)
