a1d494e59c1b2e1a98dc9ce97c38cef1e0163c5d
Vera Kahn <verakahn@nypl.org>
2022-12-12 11:40:42 -0500

    start es client tests

--- test/es-client.test.js
+++ test/es-client.test.js
@@ -1,8 +1,33 @@
+const chai = require('chai')
+chai.use(require('chai-as-promised'))
+const sinon = require('sinon')
+const expect = chai.expect
+chai.use(require('sinon-chai'))
 const esClient = require('../lib/elastic-search/client')
 
 describe('elastic search', () => {
+  process.env.ELASTICSEARCH_CONNECTION_URI = 'thebomb.com'
   describe('client', () => {
-    it()
+    let client
+    it('creates a client', async () => {
+      client = await esClient.internal._client()
+      expect(client)
+    })
+    it('returns previously created client', async () => {
+      const clientAgain = await esClient.internal._client()
+      expect(clientAgain).to.equal(client)
+    })
   })
-  describe('inde')
+  describe('_indexGeneric', () => {
+    const records = [12345, 23456, 34567].map((uri) => ({ uri, _type: 'resource', _parent: 'mom' }))
+    const clientStub = sinon.stub(esClient.internal, '_client').resolves({ bulk: (body) => Promise.resolve(body) })
+    it('builds index statement', async () => {
+      await esClient.internal._indexGeneric('indexName', records, true)
+      expect(clientStub).to.have.been.calledWith()
+    })
+    it('adds updatedAt property', () => {
+
+    })
+    it('updates or indexes as expected')
+  })
 })
