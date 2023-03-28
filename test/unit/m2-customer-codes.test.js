3c13334866f89e38ab248c2b012deafe8ebe5f0e
Vera Kahn <verakahn@nypl.org>
2023-03-28 11:34:27 -0400

    incorporate m2 codes and tests

---
+++ test/unit/m2-customer-codes.test.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect
+const sinon = require('sinon')
+const platformApi = require('../../lib/platform-api/requests')
+const { attachM2CustomerCodes } = require('../../lib/utils/m2-customer-codes')
+
+describe('attachM2CustomerCodes', () => {
+  before(() => {
+    const m2CustomerCodesApiStub = () => ({ 123: 'XX', 456: 'YY' })
+    // stub m2 service to return m2codes for barcodes
+    sinon.stub(platformApi, 'm2CustomerCodesForBarcodes')
+      .callsFake(m2CustomerCodesApiStub)
+  })
+  after(() => {
+    platformApi.m2CustomerCodesForBarcodes.restore()
+  })
+  it.only('attaches m2 codes', async () => {
+    const bib = { items: () => [{ location: { code: 'mal9' }, barcode: '123' }, { location: { code: 'mal9' }, barcode: '456' }] }
+    const bibWithM2Codes = await attachM2CustomerCodes(bib)
+    expect(bibWithM2Codes._items.map(item => item.m2CustomerCode)).to.deep.equal(['XX', 'YY'])
+  })
+})
