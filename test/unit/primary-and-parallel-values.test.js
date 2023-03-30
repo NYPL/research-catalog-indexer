6f3c6b0635fb1f46c999f443bff3e13224395b31
Vera Kahn <verakahn@nypl.org>
2023-03-30 10:19:36 -0400

    wrap up primary and parallels tests

--- test/unit/primary-and-parallel-values.test.js
+++ test/unit/primary-and-parallel-values.test.js
@@ -5,14 +5,35 @@ const SierraBib = require('../../lib/sierra-models/bib')
 const { BibMappings } = require('../../lib/mappings/mappings')
 
 describe('primary and parallel values', () => {
+  let bib
+  before(() => {
+    bib = new SierraBib(require('../fixtures/bib-11606020.json'))
+  })
   describe('primaryValues', () => {
     it('should get primary values for given mappings from a model', () => {
-      const bib = new SierraBib(require('../fixtures/bib-11606020.json'))
       const mappings = BibMappings.get('title', bib)
       expect(primaryValues(bib.varFieldsMulti(mappings))).to.deep.equal(
         ['Sefer Toldot Yeshu = The gospel according to the Jews, called Toldoth Jesu : the generations of Jesus, now first translated from the Hebrew.'
         ]
       )
+    })
+    it('should return empty array when field is not on bib', () => {
+      const mappings = BibMappings.get('seriesStatement', bib)
+      console.log(primaryValues(bib.varFieldsMulti(mappings)))
+      expect(primaryValues(bib.varFieldsMulti(mappings))).to.deep.equal([])
+    })
+    it('should return empty string array for orphaned parallel with no .value', () => {
+      const mappings = BibMappings.get('creatorLiteral', bib)
+      expect(primaryValues(bib.varFieldsMulti(mappings))).to.deep.equal([''])
+    })
+    it('orphaned parallel subfields and primary subfields with parallels', () => {
+      // This bib has a single primary 200 with a linked parallel and one orphaned parallel:
+      bib = new SierraBib(require('../fixtures/bib-parallels-chaos.json'))
+      const mappings = [{ marc: '200', subfields: ['a', 'b'] }]
+      expect(primaryValues(bib.varFieldsMulti(mappings))).to.deep.equal([
+        '200 primary value a 200 primary value b',
+        ''
+      ])
     })
   })
 
@@ -40,26 +61,19 @@ describe('primary and parallel values', () => {
 
       // This bib has a single primary 600 with a linked parallel that is tagged RTL
       mappings = BibMappings.get('subjectLiteral', bib)
-      expect(primaryValues(bib.varFieldsMulti(mappings))).to.deep.equal(['600 primary value a 600 primary value b'])
       expect(parallelValues(bib.varFieldsMulti(mappings))).to.deep.equal(['\u200F600 parallel value a 600 parallel value b'])
 
       // This bib has a single orphaned parallel for marc 100 that is tagged RTL
       mappings = BibMappings.get('creatorLiteral', bib)
-      expect(primaryValues(bib.varFieldsMulti(mappings))).to.deep.equal([''])
       expect(parallelValues(bib.varFieldsMulti(mappings))).to.deep.equal(['\u200F100 parallel value a 100 parallel value b'])
 
-      // This bib has a single primary 200 with a linked parallel and one orphaned parallel:
-      mappings = [{ marc: '200', subfields: ['a', 'b'] }]
-      expect(primaryValues(bib.varFieldsMulti(mappings))).to.deep.equal([
-        '200 primary value a 200 primary value b',
-        ''
-      ])
       // Note that we're ordering the orphaned parallel last even though its $u
       // has a subfield 6 number ('02') that is less than the non-orphaned
       // parallel's subfield 6 number ('03'). Ideeally we would present lower-
       // number values before higher value numbers, but orphaned parallels are
       // rare enough that just including their value at the end of the array is
       // sufficient for now.
+      mappings = [{ marc: '200', subfields: ['a', 'b'] }]
       expect(parallelValues(bib.varFieldsMulti(mappings))).to.deep.equal([
         '\u200F200 parallel value a 200 parallel value b',
         '\u200F200 orphaned parallel value a 200 orphaned parallel value b'
