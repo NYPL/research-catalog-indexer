0fddf85cfafd4d100aed63f7aa21b4cbaf29e21d
Vera Kahn <verakahn@nypl.org>
2023-04-20 11:18:56 -0400

    add language lookup csv

--- test/unit/lookup.test.js
+++ test/unit/lookup.test.js
@@ -4,7 +4,7 @@ const { lookup } = require('../../lib/utils/lookup')
 
 describe('lookup', () => {
   it('builds a lookup', () => {
-    expect(lookup('language-code-to-label')).to.deep.include({
+    expect(lookup('lookup-language-code-to-label')).to.deep.include({
       aar: 'Afar',
       ach: 'Acoli',
       // ...
