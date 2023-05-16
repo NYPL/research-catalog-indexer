6e5d9e485615abf27644c1d6343a77a1cd7c2b65
Vera Kahn <verakahn@nypl.org>
2023-05-16 14:21:20 -0400

    most holdings functions done

--- test/unit/es-holding.test.js
+++ test/unit/es-holding.test.js
@@ -29,18 +29,60 @@ describe('EsHolding model', () => {
   describe('format', () => {
     it('returns format from field tag i', () => {
       const holding = new EsHolding(new SierraHolding(require('../fixtures/holding-1044923.json')))
-      expect(holding.format()).to.equal('PRINT   JULY 10,1999-FEB 24 2001.')
+      expect(holding.format()).to.deep.equal(['PRINT   JULY 10,1999-FEB 24 2001.'])
     })
     it('returns format from varfield 843$a', () => {
       const holding = new EsHolding(new SierraHolding(require('../fixtures/holding-1089484.json')))
-      expect(holding.format()).to.equal('PRINT     ')
+      expect(holding.format()).to.deep.equal(['PRINT     '])
     })
   })
 
   describe('holdingStatement', () => {
     it('returns holdingStatement', () => {
+      const holding = new EsHolding(new SierraHolding(require('../fixtures/holding-1032862.json')))
+      expect(holding.holdingStatement()).to.deep.equal([
+        ['1 v. no. (year) (month)', '1.1   2012 01/02'],
+        ['1 v. no. (year) (month)', '1.1   2012 01/02']
+      ])
+    })
+  })
+
+  describe('identifier', () => {
+    const holding = new EsHolding(new SierraHolding(require('../fixtures/holding-1032862.json')))
+    it('returns shelfMark object', () => {
+      expect(holding.identifier()).to.deep.equal([{ value: 'Sc Ser.-M .N489', type: 'bf:shelfMark' }])
+    })
+  })
+
+  describe('location', () => {
+    const holding = new EsHolding(new SierraHolding(require('../fixtures/holding-1089484.json')))
+    it('returns location for valid sierra code', () => {
+      expect(holding.location()).to.deep.equal([{ id: 'loc:slrb1', label: 'Science, Industry and Business Library (SIBL) - Reference' }])
+    })
+    it('returns null for unrecognized location', () => {
+      const holding = new EsHolding(new SierraHolding({ location: { code: 'not a sierra code' } }))
+      expect(holding.location()).to.equal(null)
+    })
+  })
+
+  describe('physicalLocation', () => {
+    it('returns shelfMark', () => {
       const holding = new EsHolding(new SierraHolding(require('../fixtures/holding-1089484.json')))
-      console.log(holding.holdingStatement())
+      expect(holding.physicalLocation()).to.deep.equal(holding.shelfMark())
+    })
+  })
+
+  describe('shelfMark', () => {
+    it('returns shelfMark', () => {
+      const holding = new EsHolding(new SierraHolding(require('../fixtures/holding-1032862.json')))
+      expect(holding.shelfMark()).to.deep.equal(['Sc Ser.-M .N489'])
+    })
+  })
+
+  describe('uri', () => {
+    it('returns prefixed uri', () => {
+      const holding = new EsHolding(new SierraHolding({ id: '12345678' }))
+      expect(holding.uri()).to.equal('h12345678')
     })
   })
 })
