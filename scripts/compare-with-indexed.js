831058f1074166b9aee0639064eadd0591f900d6
Vera Kahn <verakahn@nypl.org>
2023-05-23 13:40:29 -0400

    first pass at compare script

--- scripts/compare-with-indexed.js
+++ scripts/compare-with-indexed.js
@@ -1,90 +1,55 @@
 /**
  *
- * Given an event file with a single record, generates the ES doc using local code and then
+ * Given a bib uri, generates the ES doc using local code and then
  * fetches the same record from the remote index to perform a comparison.
+ * When true is provided for active-index, the script  will query the index
+ * that is updated by the DHI. Otherwise, it will go to the research-catalog-indexer
+ * test index.
  *
  * Usage:
- *   node scripts/compare-with-indexed --envfile [path to .env] ./test/sample-events/[eventfile]')
- * Or, to fetch by id:
- *   node scripts/compare-with-indexed --envfile [path to .env] --uri [bnum]
- *
- * e.g. To compare how this app generates an ES doc for b10578183 with the QA ES index:
- *   node scripts/compare-with-indexed.js --envfile config/qa.env test/sample-events/b10578183.json
- *
- * If event file contains multiple records, only the first is compared by default. Indicate which via:
- *   --record N (default 0)
+ *   node scripts/compare-with-indexed --envfile [path to .env] --uri [bnum] --activeIndex [boolean]
  */
 
 const argv = require('minimist')(process.argv.slice(2))
 const dotenv = require('dotenv')
 dotenv.config({ path: argv.envfile || './config/qa.env' })
 
-const fs = require('fs')
-
 const NyplSourceMapper = require('discovery-store-models/lib/nypl-source-mapper')
-const index = require('../index')
-const { awsInit, die, suppressIndexAndStreamWrites } = require('../lib/script-utils')
-const { printDiff } = require('../test/diff-report')
-const platformApi = require('../lib/platform-api')
-const discoveryApiIndexer = require('../lib/discovery-api-indexer')
-const discoveryStoreModel = require('../lib/discovery-store-model')
+const { awsInit, die, printDiff } = require('./utils')
+const { bibById } = require('../lib/platform-api/requests')
+const { buildEsDocument } = require('../lib/build-es-document')
+const { currentDocument } = require('../lib/elastic-search/requests')
 
 const logger = require('../lib/logger')
 logger.setLevel(process.env.LOGLEVEL || 'info')
 
 const usage = () => {
-  console.log('Usage: node scripts/compare-with-indexed --envfile [path to .env] [--uri bnum] ./test/sample-events/[eventfile]')
+  console.log('Usage: node scripts/compare-with-indexed --envfile [path to .env] [--uri bnum]')
   return true
-}
-
-// Make simple lambda callback
-const cb = (e, result) => {
-  if (e) console.error('Error: ' + e)
-  console.log('Success: ' + result)
 }
 
 // Ensure we're looking at the right profile and region
 awsInit()
 
-// Suppress writing to ES index and Kinesis streams
-suppressIndexAndStreamWrites({
-  // Capture attempt to write document to index so we can compare it with the
-  // version actually in the index:
-  onIndexWrite: (records) => {
-    const newRecord = records[0]
+let indexName = process.env.ELASTIC_RESOURCES_INDEX_NAME
+if (!argv.uri) usage() && die('Must specify event file or uri')
+if (argv.activeIndex === 'true') indexName = process.env.HYBRID_ES_INDEX
 
-    discoveryApiIndexer.currentDocument(records[0].uri).then((liveRecord) => {
-      printDiff(liveRecord, newRecord)
-    }).catch((e) => {
-      console.log('Error: ', e)
+const { id, type, nyplSource } = NyplSourceMapper.instance().splitIdentifier(argv.uri)
+
+const buildLocalEsDocFromUri = async (nyplSource, id) => {
+  const bib = await bibById(nyplSource, id)
+  return buildEsDocument({ type: 'Bib', records: [bib] })
+}
+
+if (type === 'bib') {
+  Promise.all([buildLocalEsDocFromUri(nyplSource, id), currentDocument(argv.uri, indexName)])
+    .then(([[localEsRecord], liveEsRecord]) => {
+      printDiff(liveEsRecord, localEsRecord)
+    }).catch(e => {
+      logger.error(e.message)
+      die()
     })
-  }
-})
-
-// Insist on an eventfile or a uri:
-if (argv._.length < 1 && !argv.uri) usage() && die('Must specify event file or uri')
-
-const ev = argv._[0] ? JSON.parse(fs.readFileSync(argv._[0], 'utf8')) : null
-
-if (ev) {
-  // Invoke the lambda handler on the event
-  index.handler(ev, {}, cb)
-    .then((result) => {
-      console.log('All done')
-    })
-    .catch((e) => {
-      console.log(e)
-      console.error('Error: ', JSON.stringify(e, null, 2))
-    })
-} else if (argv.uri) {
-  const { id, type, nyplSource } = NyplSourceMapper.instance().splitIdentifier(argv.uri)
-  switch (type) {
-    case 'bib':
-      platformApi.bibById(nyplSource, id)
-        .then((bib) => {
-          return discoveryStoreModel.filterOutAndDeleteNonResearchBibs([bib])
-            .then(index.fullRebuildForBibs)
-        })
-      break
-  }
+} else {
+  die(`Only configured for bib uris, ${type} uri specified`)
 }
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,4 @@
+const { diff, detailedDiff } = require('deep-object-diff')
 const aws = require('aws-sdk')
 
 const awsInit = (profile) => {
@@ -16,7 +17,80 @@ const die = (message) => {
   process.exit()
 }
 
+function removeEmpty (obj) {
+  return Object.fromEntries(
+    Object.entries(obj)
+      .filter(([_, v]) => v != null)
+      .map(([k, v]) => [k, v === Object(v) ? removeEmpty(v) : v])
+  )
+}
+
+function logObject (obj, indent = 2) {
+  obj = removeEmpty(obj)
+  let s = JSON.stringify(
+    obj,
+    (k, v) => v === undefined ? null : v,
+    2
+  )
+  s = s.split('\n')
+    .map((line) => `${' '.repeat(indent)}${line}`)
+    .join('\n')
+  console.log(s)
+}
+
+function printDiff (orig, updated) {
+  console.log(`Bib metadata diff for ${orig.uri}:`)
+  const noChildren = (bib) => Object.assign({}, bib, { items: null, holdings: null, uris: null, updatedAt: null })
+  if (Object.keys(diff(noChildren(orig), noChildren(updated))).length > 0) {
+    const metaDiff = detailedDiff(noChildren(orig), noChildren(updated))
+    logObject(metaDiff)
+  } else {
+    console.log('  No diff')
+  }
+
+  ;['items', 'holdings'].forEach((prop) => {
+    console.log(`${prop[0].toUpperCase() + prop.substring(1)} diff:`)
+
+    if (orig[prop] || updated[prop]) {
+      if (!orig[prop]) console.log(`  No ${prop} in orig (${updated[prop].length} ${prop} in updated doc)`)
+      else if (!updated[prop]) console.log(`  No ${prop} in updated doc (${orig[prop].length} ${prop} in orig doc)`)
+      else if (orig[prop].length !== updated[prop].length) {
+        console.log(`  Orig ${prop}: ${orig[prop].length}, new ${prop} length: ${updated[prop].length}`)
+      } else {
+        console.log(`  Both documents have ${orig[prop].length} ${prop}`)
+      }
+
+      if (orig[prop] && updated[prop]) {
+        orig[prop].forEach((origItem) => {
+          if (!updated[prop]) {
+            console.log(`  Can not find ${prop} in updated doc`)
+            return
+          }
+          const updatedItem = updated[prop].find((i) => i.uri === origItem.uri)
+          if (!updatedItem) console.log(`  Can not find ${origItem.uri} in ${prop} in updated doc`)
+          else {
+            if (Object.keys(diff(origItem, updatedItem)).length > 0) {
+              const itemDiff = detailedDiff(origItem, updatedItem)
+              console.log(`  Diff in ${prop} ${origItem.uri}:`)
+              logObject(itemDiff, 4)
+            }
+          }
+        })
+      }
+    }
+  })
+
+  // console.log("Difference between live and new record:")
+  // console.log(JSON.stringify(diff(liveRecord, newRecord), null, 2))
+  // console.log("Deletions:")
+  // console.log(JSON.stringify(deletedDiff(liveRecord, newRecord), null, 2))
+
+  // console.log(JSON.stringify(addedDiff(liveRecord, records[0]), null, 2))
+  // console.log(JSON.stringify(detailedDiff(liveRecord, records[0]), null, 2))
+}
+
 module.exports = {
   awsInit,
-  die
+  die,
+  printDiff
 }
