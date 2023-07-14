bacca22d047709117f4a69b76cdda835eab20110
Vera Kahn <verakahn@nypl.org>
2023-07-14 11:18:30 -0400

    fix .sh file

--- provisioning/package.sh
+++ provisioning/package.sh
@@ -4,6 +4,7 @@ mkdir build
 # Move required application files into build:
 cp package.json build/.
 cp package-lock.json build/.
+cp index.js
 cp -R lib build/.
 cp -R config build/.
 cp -R routes build/.
