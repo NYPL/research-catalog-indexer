32bcf45934eda00d5b8a795de108cc43df90cee7
Vera Kahn <verakahn@nypl.org>
2023-07-14 14:46:59 -0400

    update tests for schema edits

--- test/unit/es-index-schema.test.js
+++ test/unit/es-index-schema.test.js
@@ -5,6 +5,6 @@ describe('elastic search schema', () => {
   it('returns the schema', () => {
     const properties = Object.keys(schema())
     expect(properties).to.include('carrierType')
-    expect(properties.length).to.equal(84)
+    expect(properties.length).to.equal(88)
   })
 })
