4d6d9e4a8f8640342c0a6f464b56aaea64ec1c74
Vera Kahn <verakahn@nypl.org>
2023-08-07 14:14:51 -0400

    create script for getting n uris

--- scripts/index-n-bibs/get-uris.js
+++ scripts/index-n-bibs/get-uris.js
@@ -1,11 +1,14 @@
-const { client } = require('../lib/elastic-search/client')
+const { client } = require('../../lib/elastic-search/client')
 
-const { searchTerms, query } = require('./index-utils')
+const { searchTerms, query } = require('./index-n-bibs-utils')
 
 const dotenv = require('dotenv')
+const fs = require('fs')
 dotenv.config({ path: './config/qa.env' })
 
-const reindex = async (maxNumUris = 10000) => {
+const NUM_URIS = 10000
+
+const index = async () => {
   const esClient = await client()
   const index = process.env.HYBRID_ES_INDEX
   const results = await Promise.all(
@@ -22,10 +25,8 @@ const reindex = async (maxNumUris = 10000) => {
     })
   )
   const ids = results.map((result) => result.hits.hits.map((hit) => hit._id)).flat()
-  return [...new Set(ids)].slice(0, maxNumUris)
+  const dedupedIds = [...new Set(ids)].slice(0, NUM_URIS).join('\n')
+  fs.writeFile('scripts/index-n-bibs/uris.csv', dedupedIds, (e) => { if (e) throw e })
 }
 
-reindex()
-  .then((uris) => {
-    console.log(uris)
-  })
+index().then(() => console.log(`wrote ${NUM_URIS} uris`))
