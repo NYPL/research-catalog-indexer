1b680afaa318d57ce4dbac3fa71d2c91c144c992
Vera Kahn <verakahn@nypl.org>
2025-06-17 16:21:55 -0400

    stub emit method

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { suppressBibs } = require('./lib/utils/suppressBibs')
 const { buildEsDocument } = require('./lib/build-es-document')
 const { truncate } = require('./lib/utils')
 const { notifyDocumentProcessed } = require('./lib/streams-client')
+const { emitCountEvents, buildBibSubjectCountEvents } = require('./lib/browse-terms')
 
 /**
  * Main lambda handler receiving Bib, Item, and Holding events
@@ -35,10 +36,11 @@ const processRecords = async (type, records, options = {}) => {
 
   const messages = []
 
-  // if (type === 'bib') {
-  //   const subjects = await buildBibSubjectCountEvents([...recordsToIndex, ...recordsToDelete])
-  //   emitCountsForSubjects(subjects)
-  // }
+  if (type === 'bib') {
+    const subjects = await buildBibSubjectCountEvents([...recordsToIndex, ...recordsToDelete])
+    await emitCountEvents(subjects)
+  }
+
   if (recordsToIndex.length) {
     if (options.dryrun) {
       logger.info(`DRYRUN: Skipping writing ${recordsToIndex.length} records`)
