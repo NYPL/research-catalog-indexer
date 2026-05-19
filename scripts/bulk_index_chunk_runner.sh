2fd84f844c4b8acefb6c15e0cc0a92f83572c369
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-05-19 13:45:12 -0400

    Emma trying to fix this chunk runner script

--- scripts/bulk_index_chunk_runner.sh
+++ scripts/bulk_index_chunk_runner.sh
@@ -2,22 +2,25 @@
 # $1  - environment (qa or production)
 # $@  -  rest of flags passed to script
 
+ENV=$1
+shift
+
 SCRIPT_DIR=.
 mkdir -p $SCRIPT_DIR/tmp/
 BIB_IDS_DIR=$SCRIPT_DIR/tmp/bib_ids/
 ERRORS_DIR=$SCRIPT_DIR/tmp/chunk_run_errors/
-PROCESSED_IDS_DIR = $SCRIPT_DIR/tmp/processed/
+PROCESSED_IDS_DIR=$SCRIPT_DIR/tmp/processed/
 mkdir -p $BIB_IDS_DIR
 mkdir -p $ERRORS_DIR
 mkdir -p $PROCESSED_IDS_DIR
 
-set -a; source config/"$1"-bulk-index.env; set +a
-DECRYPTED_PW=$(kms-util decrypt $BIB_SERVICE_DB_PW)
-DECRYPTED_HOST=$(kms-util decrypt $BIB_SERVICE_DB_HOST)
-DECRYPTED_USER=$(kms-util decrypt $BIB_SERVICE_DB_USER)
+set -a; source config/"$ENV"-bulk-index.env; set +a
+DECRYPTED_PW=$(kms-util decrypt $BIB_SERVICE_DB_PW) || { echo "Error: Failed to decrypt BIB_SERVICE_DB_PW"; exit 1; }
+DECRYPTED_HOST=$(kms-util decrypt $BIB_SERVICE_DB_HOST) || { echo "Error: Failed to decrypt BIB_SERVICE_DB_HOST"; exit 1; }
+DECRYPTED_USER=$(kms-util decrypt $BIB_SERVICE_DB_USER) || { echo "Error: Failed to decrypt BIB_SERVICE_DB_USER"; exit 1; }
 
-echo Fetching all bib ids from $1 bib database
-psql postgresql://$DECRYPTED_USER:$DECRYPTED_PW@$DECRYPTED_HOST/bib_service_production -c "\\COPY (SELECT id, nypl_source FROM bib) TO $SCRIPT_DIR/tmp/all_bib_ids.csv WITH CSV DELIMITER ',' HEADER;" 
+echo Fetching all bib ids from $ENV bib database
+psql postgresql://$DECRYPTED_USER:$DECRYPTED_PW@$DECRYPTED_HOST/bib_service_production -c "\\COPY (SELECT id, nypl_source FROM bib ORDER BY nypl_source) TO $SCRIPT_DIR/tmp/all_bib_ids.csv WITH CSV DELIMITER ',' HEADER;" 
 
 echo Splitting `wc -l $SCRIPT_DIR/tmp/all_bib_ids.csv` into files of 1000 ids
 split -l 1000 -a 5 -d $SCRIPT_DIR/tmp/all_bib_ids.csv $BIB_IDS_DIR/bib_ids_
@@ -29,7 +32,7 @@ echo commencing bulk reingest for all ids
 
 for file in `ls $BIB_IDS_DIR`; do
     echo "Processing $file"
-    if ! node ./scripts/bulk-index.js "$@" --batchSize 1000 --type bib --envfile ./config/$1-bulk-index.env --skipDeletes --csv $BIB_IDS_DIR/$file --csvIdColumn 0 --csvNyplSourceColumn 1; then
+    if ! node ./scripts/bulk-index.js "$@" --batchSize 1000 --type bib --envfile ./config/$ENV-bulk-index.env --skipDeletes --csv $BIB_IDS_DIR/$file --csvIdColumn 0 --csvNyplSourceColumn 1; then
       echo "csv file failed run: $file"
       mv $BIB_IDS_DIR/$file $ERRORS_DIR
     fi
