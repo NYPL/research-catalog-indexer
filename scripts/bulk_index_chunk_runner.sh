e7c5f69f98c60e346f3d348489d3f024ecb800aa
Vera Kahn <verakahn@nypl.org>
2026-05-26 15:25:17 -0400

    linting chunk_runner

--- scripts/bulk_index_chunk_runner.sh
+++ scripts/bulk_index_chunk_runner.sh
@@ -8,12 +8,14 @@ mkdir -p $SCRIPT_DIR/tmp/
 BIB_IDS_DIR=$SCRIPT_DIR/tmp/bib_ids/
 ERRORS_DIR=$SCRIPT_DIR/tmp/chunk_run_errors/
 PROCESSED_IDS_DIR=$SCRIPT_DIR/tmp/processed/
+FETCH_IDS=$2
+
 mkdir -p $BIB_IDS_DIR
 mkdir -p $ERRORS_DIR
 mkdir -p $PROCESSED_IDS_DIR
 
 if [ "$2" == "true" ]; then
-  set -a; source config/"$1"-bulk-index.env; set +a
+  source config/"$1"-bulk-index.env; set +a
   DECRYPTED_PW=$(kms-util decrypt $BIB_SERVICE_DB_PW)
   DECRYPTED_HOST=$(kms-util decrypt $BIB_SERVICE_DB_HOST)
   DECRYPTED_USER=$(kms-util decrypt $BIB_SERVICE_DB_USER)
@@ -21,7 +23,7 @@ if [ "$2" == "true" ]; then
 	psql postgresql://$DECRYPTED_USER:$DECRYPTED_PW@$DECRYPTED_HOST/bib_service_production -c "\\COPY (SELECT id, nypl_source FROM bib) TO $SCRIPT_DIR/tmp/all_bib_ids.csv WITH CSV DELIMITER ',' HEADER;" 
 
 	echo Splitting `wc -l $SCRIPT_DIR/tmp/all_bib_ids.csv` into files of 1000 ids
-split -l 1000 -a 5 -d $SCRIPT_DIR/tmp/all_bib_ids.csv $BIB_IDS_DIR/bib_ids_
+  split -l 1000 -a 5 -d $SCRIPT_DIR/tmp/all_bib_ids.csv $BIB_IDS_DIR/bib_ids_
 
 	echo Deleting original $SCRIPT_DIR/tmp/all_bib_ids.csv file
 	rm $SCRIPT_DIR/tmp/all_bib_ids.csv 
