0e2035fc1864a1751023761e789e0b598032f81c
danamansana <danielappel@nypl.org>
2026-08-13 15:51:00 -0400

    Replace wc with awk

--- scripts/utils.js
+++ scripts/utils.js
@@ -351,7 +351,7 @@ const lineCount = (file) => {
   const exec = require('child_process').exec
 
   return new Promise((resolve, reject) => {
-    exec(`wc -l ${file}`, (error, results) => {
+    exec(`awk 'END {print NR}' ${file}`, (error, results) => {
       if (error) return reject(error)
 
       const count = parseInt(
