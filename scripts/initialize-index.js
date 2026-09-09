20143b3d84577e4420d450e462a1fb79a6558195
Vera Kahn <verakahn@nypl.org>
2026-09-09 12:22:13 -0400

    update inititalize-index script to not wait for completion, use schema keys

--- scripts/initialize-index.js
+++ scripts/initialize-index.js
@@ -58,8 +58,9 @@ const optionallyCopyContentsToNewIndex = async (newIndexName) => {
     if (answer === 'yes') {
       console.log(`Copying contents of ${oldIndex} to ${newIndexName}`)
       const resp = await client.reindex({
+        wait_for_completion: false, 
         body: {
-          source: { index: oldIndex },
+          source: { index: oldIndex, "_source": Object.keys(schema())},
           dest: { index: newIndexName }
         }
       })
