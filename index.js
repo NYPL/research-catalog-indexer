a469d5b80f4ecff54188ec3068f7231ed3d16ffc
Vera Kahn <verakahn@nypl.org>
2026-09-24 14:40:46 -0400

    use promise.allsettled

--- index.js
+++ index.js
@@ -90,8 +90,7 @@ const processRecords = async (type, records, options = {}) => {
   if (process.env.EMIT_BROWSE_TERMS === 'true') {
     // emit for deleted records as well
     const allEsDocuments = esModels.concat(removedBibs.map(bib => new EsBib(new SierraBib(bib))))
-    await browse.emitBrowseTerms(allEsDocuments, 'subject')
-    await browse.emitBrowseTerms(allEsDocuments, 'contributor')
+    await Promise.allSettled[browse.emitBrowseTerms(allEsDocuments, 'subject'), browse.emitBrowseTerms(allEsDocuments, 'contributor')]
   }
   const message = messages.length ? messages.join('; ') : 'Nothing to do.'
 
